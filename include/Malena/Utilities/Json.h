4e656e8980d2f6e35220fdc7ddf592637ccb7558
dsmizzle <daveryaaziem@hotmail.com>
2026-04-17 16:14:52 -0700

    many modifications to admin and student

---
+++ include/Malena/Utilities/Json.h
@@ -0,0 +1,14 @@
+//
+// Created by Dave Smith on 4/17/26.
+//
+
+#ifndef JSON_H
+#define JSON_H
+#include <nlohmann/json_fwd.hpp>
+#include <nlohmann/detail/meta/std_fs.hpp>
+
+namespace ml
+{
+    using json = nlohmann::json;
+}
+#endif //JSON_H
